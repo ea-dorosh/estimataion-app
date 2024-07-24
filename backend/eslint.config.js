99733eefe40f049332902c41ee858df1749552fb
egordorosh <egordororsh@gmail.com>
2024-07-24 18:09:55 +0200

    add eslint for backend

---
+++ backend/eslint.config.js
@@ -0,0 +1,27 @@
+// eslint.config.js
+
+import js from '@eslint/js';
+import node from 'eslint-plugin-node';
+
+export default [
+  js.configs.recommended,
+  {
+    files: [`**/*.js`],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: `module`,
+      globals: {
+        require: `readonly`,
+        console: `readonly`,
+      },
+    },
+    plugins: {
+      node,
+    },
+    rules: {
+      'indent': [`error`, 2],
+      'node/no-unsupported-features/es-syntax': `off`,
+      'quotes': [`error`, `backtick`],
+    },
+  },
+];

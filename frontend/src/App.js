1e80e98ea0e16381b8ebe364b964069c3ac2ecdc
egordorosh <egordororsh@gmail.com>
2024-08-01 20:54:47 +0200

    add server url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Admin from './pages/Admin/Admin';
 import style from './app.module.scss';
 import io from 'socket.io-client';
 
-const socket = io(`api/`);
+const socket = io(`https://estimataion-app.onrender.com/`);
 
 function App() {
   return (
